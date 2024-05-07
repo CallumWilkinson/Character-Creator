597b93d3df6838fc843269df06fad11fdb058bad
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-05-07 15:22:34 +1000

    added character model

--- Character-Creator/Models/Character.cs
+++ Character-Creator/Models/Character.cs
@@ -4,9 +4,17 @@ namespace Character_Creator.Models
 {
     public class Character
     {
-        public int Id { get; set; }
+        public int CharacterId { get; set; }
 
         [MaxLength(20)]
         public string Name { get; set; } = "";
+
+        [MaxLength(20)]
+        public string Race { get; set; } = "";
+
+        [MaxLength(20)]
+        public string Class { get; set; } = "";
+
+        public int Level { get; set; }
     }
 }
