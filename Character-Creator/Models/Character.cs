fb603cff3e343443c1c28f7689a6978d971b88d0
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-05-07 15:11:18 +1000

    update

---
+++ Character-Creator/Models/Character.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Character_Creator.Models
+{
+    public class Character
+    {
+        public int Id { get; set; }
+
+        [MaxLength(20)]
+        public string Name { get; set; } = "";
+    }
+}
---
+++ Character-Creator/Services/DataService.cs
@@ -0,0 +1,15 @@
+﻿using System.Data;
+using Microsoft.Data.Sqlite;
+using Dapper;
+
+namespace Character_Creator.Services;
+
+public class DataService
+{
+    private readonly string _connectionString;
+
+    public DataService(IConfiguration configuration)
+    {
+        _connectionString = configuration.GetConnectionString("DefaultConnection");
+    }
+}
