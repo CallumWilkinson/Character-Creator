d7d316ba4b127d9131268284f33087edac3e4822
CallumWilkinson <84135636+CallumWilkinson@users.noreply.github.com>
2024-05-23 16:05:11 +1000

    Create page - character name
    create button links to create page and now shows name input, user inputs name, character is created and adds to db

--- Character-Creator/Pages/CreateCharacter.cshtml.cs
+++ Character-Creator/Pages/CreateCharacter.cshtml.cs
@@ -9,6 +9,9 @@ namespace Character_Creator.Pages
     {
         private readonly DataService _dataBase;
 
+        [BindProperty]
+        public Character Character { get; set; } = new Character();
+
         public CreateCharacterModel(DataService dataBase)
         {
             _dataBase = dataBase;
@@ -20,6 +23,23 @@ namespace Character_Creator.Pages
         public void OnPost() 
         {
             //save the new character to the database
+            Character character = new Character()
+            {
+                Name = Character.Name,
+                Race = Character.Race,
+                Class = Character.Class,
+                Level = Character.Level,
+            };
+
+            _dataBase.AddCharacter(character);
+            
+
+
+
+
+
+
+
 
             //i think i need to use [bindpropery] on the character object so that when user enters name ect in the form it automatically maps that to the characer object
            
